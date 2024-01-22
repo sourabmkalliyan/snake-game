2f56a5d5ed17f44947e9b3b49f41036bb42b5c50
sourabmkalliyan <sourabmkalliyanofficial@gmail.com>
2024-01-22 23:26:56 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from turtle import Turtle, Screen
+
+#Screen setup
+screen = Screen()
+screen.setup(height=600, width=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+#Creating initial segment
+segment = []
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+for position in starting_positions:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(position)
+    segment.append(new_segment)
+
+game_is_on = True
+while game_is_on:
+    for seg in segment:
+        seg.forward(20)
+
+screen.exitonclick()
