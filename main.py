7c9473295dcdc1c8d3e331111107ba88618a3551
sourabmkalliyan <sourabmkalliyanofficial@gmail.com>
2024-02-04 02:48:46 +0530

    Created a scoreboard

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from turtle import Screen
 import time
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 
 # Screen setup
 screen = Screen()
@@ -12,6 +13,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "w")
@@ -27,6 +29,7 @@ while game_is_on:
     #collisio  with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        scoreboard.increase_score()
 
 
 screen.exitonclick()
