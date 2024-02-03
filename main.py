31b37f5f27820b8d9c968de60addcd2423241580
sourabmkalliyan <sourabmkalliyanofficial@gmail.com>
2024-02-04 02:01:26 +0530

    Controlling the sanke with keypress

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "w")
+screen.onkey(snake.down, "s")
+screen.onkey(snake.left, "a")
+screen.onkey(snake.right, "d")
 game_is_on = True
 while game_is_on:
     screen.update()
