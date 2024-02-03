43150b35f21f5497f48abfca0c0098579355c47b
sourabmkalliyan <sourabmkalliyanofficial@gmail.com>
2024-02-04 03:17:55 +0530

    Detect collision with the tail

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ food = Food()
 scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(snake.up, "w")
-screen.onkey(snake.down, "s")
-screen.onkey(snake.left, "a")
-screen.onkey(snake.right, "d")
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
@@ -37,10 +37,8 @@ while game_is_on:
         game_is_on = False
 
     #Detect collision with the tail
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
             game_is_on = False
 
 scoreboard.game_over()
